b67801047910f5d34a6aac9c4625499fd0983748
Bruno Brasil <brunobalbrasil2@gmail.com>
2023-11-23 15:23:57 -0300

    final project

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,9 @@
+export const buttonPressAudio = new Audio('./assets/button-press.wav')
+buttonPressAudio.volume = 0.3
+
+export const kitchenTimer = new Audio('./assets/kitchen-timer.mp3')
+kitchenTimer.volume = 0.1
+
+export const bgAudio = new Audio('./assets/bg-audio.mp3')
+bgAudio.volume = 0.4
+bgAudio.loop = true
